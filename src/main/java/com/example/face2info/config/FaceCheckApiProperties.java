dee0f1d9b041b444f3208affedcc42fe61c2bc94
天烦 <282794269@qq.com>
2026-03-31 16:01:08 +0800

    chore(config): 同步FaceCheck默认搜索路径配置

--- src/main/java/com/example/face2info/config/FaceCheckApiProperties.java
+++ src/main/java/com/example/face2info/config/FaceCheckApiProperties.java
@@ -9,7 +9,7 @@ public class FaceCheckApiProperties {
 
     private String baseUrl = "https://facecheck.id";
     private String uploadPath = "/api/upload_pic";
-    private String searchPath = "/api/search";
+    private String searchPath = "/api/get_results";
     private String apiKey;
     private int connectTimeoutMs = 5000;
     private int readTimeoutMs = 15000;
